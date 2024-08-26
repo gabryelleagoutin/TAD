a489daae9b7b0be53d3d03d9dad0e1c508bb15e8
Gabryelle Agoutin <gabryelle.agoutin@inrae.fr>
2024-08-26 13:46:10 +0200

    correction launch_search_taxid_and_monocopy_and_percentage_calculation.sh

--- 2_search_taxid_and_monocopy_calculation/launch_search_taxid_and_monocopy_and_percentage_calculation.sh
+++ 2_search_taxid_and_monocopy_calculation/launch_search_taxid_and_monocopy_and_percentage_calculation.sh
@@ -7,4 +7,4 @@
 
 module load devel/python/Python-3.11.1
 
-python search_taxid_and_monocopy_and_percentage_calculation.py -i ../1_formatting_file_bacterian_OG/save_results/Bacterial_OG.tab -f PATH/Orthodb/odb11v0_species.tab -l PATH/Orthodb/odb11v0_level2species.tab -s [TAXID] -o OG_[TAXID].tab
+python search_taxid_and_monocopy_and_percentage_calculation.py -i ../1_formatting_file_bacterian_OG/Bacterial_OG.tab -f PATH/Orthodb/odb11v0_species.tab -l PATH/Orthodb/odb11v0_level2species.tab -s [TAXID] -o OG_[TAXID].tab

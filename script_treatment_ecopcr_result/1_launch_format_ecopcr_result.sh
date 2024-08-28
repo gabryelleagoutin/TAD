3a9a961db058037138db0c646c01b4a15fee70f7
Gabryelle Agoutin <gabryelle.agoutin@inrae.fr>
2024-08-28 11:22:25 +0200

    correction 1_launch_format_ecopcr_result.sh

--- script_treatment_ecopcr_result/1_launch_format_ecopcr_result.sh
+++ script_treatment_ecopcr_result/1_launch_format_ecopcr_result.sh
@@ -8,4 +8,4 @@
 module load devel/python/Python-3.11.1
 ml bioinfo/VSEARCH/2.22.1
 
-python /PATH/TaxonMarker/script_treatment_ecopcr_result/format_ecopcr_result.py -o . -t /home/gagoutin/work/BD_TaxonMarker/BD_ecoPCR/name_seq_with_taxo.txt primers/*ecopcr
+python /PATH/TaxonMarker/script_treatment_ecopcr_result/format_ecopcr_result.py -o . -t /PATH/BD_TaxonMarker/BD_ecoPCR/name_seq_with_taxo.txt primers/*ecopcr

07ca8369ccf5bebae5d7f033fb3d1c9ebfe506e2
Gabryelle Agoutin <gabryelle.agoutin@inrae.fr>
2024-09-23 13:13:04 +0200

    correction extension

---
+++ 6_genome_selection/launch_filter_seq_with_keywords.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+#SBATCH -J launch_filter_seq_with_keywords
+#SBATCH -p unlimitq
+#SBATCH --mem=50G
+#SBATCH -c 3
+#SBATCH --mail-type=BEGIN,END,FAIL
+
+module load devel/python/Python-3.11.1
+
+python filter_seq_with_keywords.py -s /PATH/BD_TaxonMarker/BD_ecoPCR/name_seq_with_taxo.txt -o filtered_seq_with_taxo.txt -i [included_keywords] -e [excluded_keywords]

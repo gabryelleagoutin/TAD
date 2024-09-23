585be33496c1e10993e4d7b1a48ca5f485db0ca1
Gabryelle Agoutin <gabryelle.agoutin@inrae.fr>
2024-09-23 14:19:19 +0200

    correction

--- 1_formatting_file_bacterian_OG/launch_formatting_bacterial_orthologue_file.sh
+++ 1_formatting_file_bacterian_OG/launch_formatting_bacterial_orthologue_file.sh
@@ -7,4 +7,4 @@
 
 module load devel/python/Python-3.11.1
 
-python formatting_bacterial_orthologue_file_final.py -o PATH/Orthodb/odb11v0_OG2genes.tab -b only_line_bacteria.txt -u uniq_og_ids.txt -s PATH/Orthodb/odb11v0_level2species.tab -g PATH/Orthodb/odb11v0_OGs.tab -f Bacterial_OG.tab
+python formatting_bacterial_orthologue_file.py -o PATH/Orthodb/odb11v0_OG2genes.tab -b only_line_bacteria.txt -u uniq_og_ids.txt -s PATH/Orthodb/odb11v0_level2species.tab -g PATH/Orthodb/odb11v0_OGs.tab -f Bacterial_OG.tab

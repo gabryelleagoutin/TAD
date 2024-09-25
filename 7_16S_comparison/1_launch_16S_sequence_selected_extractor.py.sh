281e4a5aea0053ffa2a302f56bc633185b182317
Gabryelle Agoutin <gabryelle.agoutin@inrae.fr>
2024-09-25 12:08:05 +0200

    add 1_launch_16S_sequence_selected_extractor.py.sh

--- 7_16S_comparison/1_launch_16S_sequence_selected_extractor.py.sh
+++ 7_16S_comparison/1_launch_16S_sequence_selected_extractor.py.sh
@@ -7,4 +7,5 @@
 
 module load devel/python/Python-3.11.1
 
+
 python 16S_sequence_selected_extractor.py --genome_file ../6_genome_selection/genome_name_selected.txt --tsv_file /PATH/BD_TaxonMarker/BD_16S/16S_with_taxonomy.tsv --fasta_file /PATH/BD_TaxonMarker/BD_16S/16S.fna --output_file filtered_16S.fna

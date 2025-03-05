9ea7df8d688b9a2d60d2876c6bb5c51d0c5275aa
Gabryelle Agoutin <gabryelle.agoutin@inrae.fr>
2025-03-05 14:46:11 +0100

    new TAD

--- 4_primer_design/script_step_by_step/6_sarray_couple_file.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-for file in result_stat_primers/concatenated_*; do echo "module load devel/python/Python-3.11.1;python couple_primer.py -i $file -f alignment/ --amplicon_min_size 150 --amplicon_max_size 590" >> tab_couple.sarray;done
